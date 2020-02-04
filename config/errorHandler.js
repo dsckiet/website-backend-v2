7394d2282dea41e2db25cce5ff94855fc513d466
Rohan <rhnmht30@gmail.com>
2020-02-04 19:54:48 +0530

    --add-dependency: log4js -> updated errorHandler.js updated .gitignore

--- config/errorHandler.js
+++ config/errorHandler.js
@@ -1,6 +1,18 @@
+const log4js = require("log4js");
+log4js.configure({
+	appenders: { cheese: { type: "file", filename: "server-logs.log" } },
+	categories: { default: { appenders: ["cheese"], level: "error" } }
+});
+let logger = log4js.getLogger();
+logger.level = "debug";
+
 module.exports.catchErrors = middlewareFunction => {
 	return (req, res, next) => {
 		middlewareFunction(req, res, next).catch(err => {
+			//log
+			logger = log4js.getLogger("Logs from catchErrors middleware");
+			logger.error(err);
+			//send to next
 			next(err);
 		});
 	};
@@ -8,6 +20,8 @@ module.exports.catchErrors = middlewareFunction => {
 
 // not found routes
 module.exports.notFound = (req, res) => {
+	logger = log4js.getLogger("Wrong endpoint request");
+	logger.info(`${req.params[0]} has been hit`);
 	res.status(404).json({
 		message: "Welcome to DSC-KIET API!! This route does not exist"
 	});
@@ -22,6 +36,8 @@ module.exports.sendErrors = (err, req, res, next) => {
 	//logging error for backend console
 	console.log(errorDetailsToSend);
 	console.log(err.stack);
+	logger = log4js.getLogger("Logs from sendErrors middleware");
+	logger.error(errorDetailsToSend);
 	//sending error to frontend
 	res.status(err.status || 500).json(errorDetailsToSend);
 };
