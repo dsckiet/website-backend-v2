4abd282c05c0ccc04eadd6c0fb0adee205c19b4c
Rohan <rhnmht30@gmail.com>
2020-02-27 17:53:45 +0530

    --refactor: user module -> remove password required true from schema since its always auto generated remove password field from req.body

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema(
 	{
 		name: { type: String, required: true },
 		email: { type: String, required: true },
-		password: { type: String, required: true },
+		password: { type: String },
 		role: {
 			type: String,
 			default: "member",
