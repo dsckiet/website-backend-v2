7fed4f87b0c8c554a8c80fe3a0b0a669866f028b
ritik srivastava <ritiksrivastava025@gmail.com>
2020-03-02 15:01:09 +0530

    --update: password made required

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema(
 	{
 		name: { type: String, required: true },
 		email: { type: String, required: true },
-		password: { type: String },
+		password: { type: String, required: true },
 		role: {
 			type: String,
 			default: "member",
