5b045eb7d9e6a442cc0a7367ca5ce0e313427a3c
Rohan <rhnmht30@gmail.com>
2020-06-15 21:54:51 +0530

    update: new deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,29 @@ read -p "Enter your server public IP : " ip
 echo "SSHing to ubuntu@$ip...Please wait!"
 ssh -i dsc-portal-key.pem ubuntu@$ip << 'ENDSSH'
 echo "Connection Established"
-cd website-backend-v2
+echo "Deploying production"
+cd ~/prod/website-backend-v2
 echo "Stopping the process.."
-sudo pm2 stop index
+sudo pm2 stop 0
+echo "Pulling changes..."
+sudo git pull origin production
+echo "Installing dependencies..."
+sudo npm i
+echo "Starting the process"
+sudo pm2 start 0
+echo "Latest production code deployed successfully!!"
+
+echo " "
+
+echo "Deploying development"
+cd ~/dev/website-backend-v2
+echo "Stopping the process.."
+sudo pm2 stop 1
 echo "Pulling changes..."
 sudo git pull origin master
 echo "Installing dependencies..."
 sudo npm i
 echo "Starting the process"
-sudo pm2 start index
-echo "Latest code deployed successfully!!"
+sudo pm2 start 1
+echo "Latest development code deployed successfully!!"
 ENDSSH
