2f0d2a15231983b1aafe3237cf274ef4ea22a3ad
ritiksr25 <ritiksr25@gmail.com>
2021-07-06 20:53:18 +0530

    --chore: apply ratelimiting for dev only

--- config/rateLimit.js
+++ config/rateLimit.js
@@ -1,5 +1,6 @@
 const RateLimit = require("express-rate-limit");
 const RedisStore = require("rate-limit-redis");
+const { NODE_ENV } = require(".");
 
 const constructRateLimitObj = (windowMs, max, message, opts) => {
 	return {
@@ -20,41 +21,62 @@ const getAuthRateLimitObj = constructRateLimitObj(
 	"Too many attempts! Try again in few hours"
 );
 
-module.exports.globalRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:global" }),
-	...constructRateLimitObj(60000, 100, "Too many requests.")
-});
+module.exports.globalRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:global" }),
+				...constructRateLimitObj(60000, 100, "Too many requests.")
+		  });
 
-module.exports.addTodoRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:addTodo" }),
-	...constructRateLimitObj(60000, 20, "Temporarily blocked")
-});
+module.exports.addTodoRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:addTodo" }),
+				...constructRateLimitObj(60000, 20, "Temporarily blocked")
+		  });
 
-module.exports.loginRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:login" }),
-	...getAuthRateLimitObj
-});
+module.exports.loginRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:login" }),
+				...getAuthRateLimitObj
+		  });
 
-module.exports.changePasswordRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:changePwd" }),
-	...getAuthRateLimitObj
-});
+module.exports.changePasswordRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:changePwd" }),
+				...getAuthRateLimitObj
+		  });
 
-module.exports.forgotPasswordRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:forgotPwd" }),
-	...getAuthRateLimitObj
-});
+module.exports.forgotPasswordRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:forgotPwd" }),
+				...getAuthRateLimitObj
+		  });
 
-module.exports.resetPasswordRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:resetPwd" }),
-	...getAuthRateLimitObj
-});
+module.exports.resetPasswordRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:resetPwd" }),
+				...getAuthRateLimitObj
+		  });
 
-module.exports.updateProfileRateLimiter = new RateLimit({
-	store: new RedisStore({ prefix: "rl:updateProfile" }),
-	...constructRateLimitObj(
-		86400000,
-		3,
-		"Too many attempts! Try again after 1 day"
-	)
-});
+module.exports.updateProfileRateLimiter =
+	NODE_ENV !== "production"
+		? (req, res, next) => next()
+		: new RateLimit({
+				store: new RedisStore({ prefix: "rl:updateProfile" }),
+				...constructRateLimitObj(
+					86400000,
+					3,
+					"Too many attempts! Try again after 1 day"
+				)
+		  });
